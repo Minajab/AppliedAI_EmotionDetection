fdbdadacd7d325bba677b8bc3e711134fa5faea9
Minamrz <99191006+Minamrz@users.noreply.github.com>
2023-10-25 13:21:51 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,33 +1,55 @@
 
-
 import os
 import cv2
-import matplotlib.pyplot as plt
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_files
 
-def visualize_dataset(data_dir, num_samples_per_class=5):
-    classes = ["Angry", "Bored", "Focused", "Neutral"]
+emotion_dataset_directory = "C:\\University\\pythonProject1\\AppliedAI_EmotionDetection\\Dataset\\"
 
-    for class_name in classes:
-        class_dir = os.path.join(data_dir, class_name)
-        files = os.listdir(class_dir)
-        sample_files = random.sample(files, min(num_samples_per_class, len(files)))
+emotion_dataset = load_files(emotion_dataset_directory, shuffle=False)
 
-        for i, file_name in enumerate(sample_files):
-            img_path = os.path.join(class_dir, file_name)
-            img = cv2.imread(img_path)
+emotions = emotion_dataset.target_names
 
-            plt.figure(figsize=(15, 3))
-            plt.subplot(1, num_samples_per_class, i + 1)
-            plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            plt.title(f'{class_name}')
-            plt.axis('off')
+class_distribution = [len(class_files) for class_files in emotion_dataset.filenames]
 
-        plt.show()
+plt.figure(figsize=(10, 5))
+plt.bar(emotions, class_distribution, color='lightblue')
+plt.xlabel('Emotion Class')
+plt.ylabel('Number of Images')
+plt.title('Distribution of Images Across Different Emotions')
+plt.xticks(rotation=45)
+plt.show()
 
-def main():
-    input_directory = "C:\\University\\pythonProject1\\AppliedAI_EmotionDetection\\Dataset\\"
-    visualize_dataset(input_directory)
+sample_images = []
+num_samples_per_emotion = 5
 
-if __name__ == "__main__":
-    main()
+for emotion in emotions:
+    emotion_dir = os.path.join(emotion_dataset_directory, emotion)
+    image_files = random.sample(os.listdir(emotion_dir), num_samples_per_emotion)
+
+    for image_file in image_files:
+        image_path = os.path.join(emotion_dir, image_file)
+        image = cv2.imread(image_path)
+        sample_images.append((image, emotion))
+
+fig, axs = plt.subplots(5, 5, figsize=(10, 10))
+for i in range(5):
+    for j in range(5):
+        img, emotion = sample_images[i * 5 + j]
+        axs[i, j].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        axs[i, j].set_title(emotion)
+        axs[i, j].axis('off')
+plt.show()
+
+for image, emotion in sample_images:
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    plt.hist(gray_image.ravel(), bins=256, range=(0, 256), density=True, alpha=0.5, label=emotion)
+
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.title('Pixel Intensity Distribution in Sample Images')
+plt.legend()
+plt.show()
+
